91d32183576bd3abdebea1da610ca72c24ec185e
onika <onikayadav00@gmail.com>
2021-09-23 00:03:16 +0530

    Update jumpsearch.cpp

---
+++ week 1/jumpsearch.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+    A:int s;
+    cin>>s;
+    int arr[20];
+    for(int i=0;i<s;i++)
+    {
+      cin>>arr[i];
+    }
+    int ul,ll;
+}
